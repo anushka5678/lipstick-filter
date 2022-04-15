e21f23f89751770edbd18d5fa36e554bfcb3d8fa
anushka5678 <78417553+anushka5678@users.noreply.github.com>
2022-04-15 16:52:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+lipstick= loadImage('https://i.postimg.cc/7YP4VqCT/Lipstick-kiss-print-isolated-on-transparent-PNG-removebg-preview.png')
 }
 
 function setup(){
@@ -20,13 +22,16 @@ function gotPoses(results)
 {
     if(results.length > 0){
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+         console.log(" nose x = " + noseX);
+         console.log(" nose y = " + noseY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(lipstick, noseX-20, noseY+17, 45, 30);
 }
 
 
